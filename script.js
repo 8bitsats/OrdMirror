a554103eb7f1678e7ac9efee4f128ac593cd88ef
8bit <8bit@recursincribe.com>
2023-10-30 14:58:05 -0400

    Update and rename java.js to script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function previewContent() {
+    const url = document.getElementById('url-input').value;
+    const contentType = document.getElementById('content-type').value;
+    const contentViewer = document.getElementById('content-viewer');
+
+    // Clear previous content
+    contentViewer.innerHTML = '';
+
+    if (url) {
+        if (contentType === 'image') {
+            const img = new Image();
+            img.src = url;
+            img.style.border = '1px solid #00ff00';
+            contentViewer.appendChild(img);
+        } else if (contentType === 'html') {
+            const iframe = document.createElement('iframe');
+            iframe.srcdoc = url;
+            iframe.width = '800';
+            iframe.height = '600';
+            iframe.style.border = '1px solid #00ff00';
+            contentViewer.appendChild(iframe);
+        } else if (contentType === 'svg') {
+            contentViewer.innerHTML = url;
+        } else if (contentType === 'p5') {
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.innerHTML = url;
+            contentViewer.appendChild(script);
+        }
+    }
+}
